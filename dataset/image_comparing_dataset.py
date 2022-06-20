8f7656e0d9da90ed28f51c9fc3894db2ba799235
long.le3 <lap02387@LAP02387s-MacBook-Pro.local>
2022-06-20 21:56:34 +0700

    run comparing image

---
+++ dataset/image_comparing_dataset.py
@@ -0,0 +1,53 @@
+from __future__ import print_function, division
+import os
+import torch
+import pandas as pd
+from skimage import io
+from torch.utils.data import Dataset
+from skimage.color import gray2rgb
+import requests
+from io import BytesIO
+
+class ImageComparingDataset(Dataset):
+
+    def __init__(self, input_file, shopee_image_key, tiki_image_key, shopee_id="id", tiki_id="tiki_ids", transform=None):
+        self.df = self.__read_input_file(input_file)
+        self.df = self.df.apply(lambda x: x.explode() if x.name in ["tiki_images", "tiki_ids"] else x)
+        self.transform = transform
+        self.shopee_image_key = shopee_image_key
+        self.tiki_image_key = tiki_image_key
+        self.shopee_id = shopee_id
+        self.tiki_id = tiki_id
+
+    def __read_input_file(self, input_file):
+        extension_input_file = os.path.splitext(input_file)[-1][1:]
+        if "csv" in extension_input_file:
+            return pd.read_csv(input_file)
+        elif "parquet" in extension_input_file:
+            return pd.read_parquet(input_file)
+        elif "json" in extension_input_file:
+            return pd.read_json(input_file)
+        return pd.DataFrame()
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        first_image = io.imread(BytesIO(requests.get(self.df.iloc[idx][self.shopee_image_key]).content))
+        second_image = io.imread(BytesIO(requests.get(self.df.iloc[idx][self.tiki_image_key].content)))
+        shopee_id = self.df.iloc[idx][self.shopee_id]
+        tiki_id = self.df.iloc[idx][self.tiki_id]
+        if len(first_image.shape) < 3:
+            first_image = gray2rgb(first_image)
+        if first_image.shape[2] == 4:
+            first_image = first_image[:, :, :3]
+        if len(second_image.shape) < 3:
+            second_image = gray2rgb(second_image)
+        if second_image.shape[2] == 4:
+            second_image = second_image[:, :, :3]
+        if self.transform:
+            first_image = self.transform(image=first_image)
+            second_image = self.transform(image=second_image)
+        return {"shopee_image": first_image, "tiki_image": second_image, "shopee_id": shopee_id, "tiki_id": tiki_id}
---
+++ image_comparing.py
@@ -0,0 +1,60 @@
+import numpy as np
+from dataset.image_comparing_dataset import ImageComparingDataset
+from transform.train_transform import get_val_transform
+from torch.utils import data
+import torch
+from modules.efficient_backbone import EfficientBackbone
+from utils.util import load_model_state_dict
+import torch.nn as nn
+import pandas as pd
+
+torch.cuda.empty_cache()
+
+if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    dataset = ImageComparingDataset("./input_file/image_comparing_v1.parquet",
+                                   "main_image_url", "tiki_images", transform=get_val_transform())
+    data_loader = data.DataLoader(dataset, batch_size=8, shuffle=False, num_workers=4)
+    model = EfficientBackbone("efficientnet-b4", False)
+    model = load_model_state_dict(model, "./checkpoints/efficientnet-b4_28.pth")
+    if torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)
+    model = model.to(device)
+    model.eval()
+    cos = nn.CosineSimilarity(dim=1)
+    similarity_df = pd.DataFrame(columns=["shopee_id", "tiki_id", "cos_sim_score"])
+    for ii, data in enumerate(data_loader):
+        try:
+            shopee_image, tiki_image, shopee_ids, tiki_ids = data["shopee_image"], data["tiki_image"], \
+                                                           data["shopee_id"], data["tiki_id"]
+            shopee_image = shopee_image.type(torch.cuda.FloatTensor)
+            tiki_image = tiki_image.type(torch.cuda.FloatTensor)
+            shopee_feature = model(shopee_image)
+            tiki_feature = model(tiki_image)
+            cosine_similarity = cos(shopee_feature, tiki_feature).cpu().detach().numpy()
+            for shopee_id, tiki_id, score in zip(shopee_ids, tiki_ids, cosine_similarity):
+                similarity_df = similarity_df.append({"shopee_id": shopee_id, "tiki_id": tiki_id, "cos_sim_score": score}, ignore_index=True)
+        except Exception as e:
+            print(e)
+            similarity_df.to_parquet("./output/similarity_score_v1.parquet")
+    # embeddings = np.concatenate(embeddings)
+    # neigh.fit(embeddings)
+    # image_distances, image_indices = neigh.kneighbors(embeddings)
+    # y_true = []
+    # y_pred = []
+    # for idx, results in enumerate(image_indices):
+    #     temp_results = set(results) - {idx}
+    #     y_true.append(dataset.df.iloc[idx]["label"])
+    #     label = int(dataset.df.iloc[idx]["label"])
+    #     correct_idx = -1
+    #     for result in temp_results:
+    #         if dataset.df.iloc[result]["label"] == label:
+    #             correct_idx = result
+    #             break
+    #     if correct_idx == -1:
+    #         y_pred.append(dataset.df.iloc[list(temp_results)[0]]["label"])
+    #     else:
+    #         y_pred.append(dataset.df.iloc[correct_idx]["label"])
+    # print(precision_score(y_true, y_pred, average="micro"))
+    # print(recall_score(y_true, y_pred, average="micro"))
+    # print(f1_score(y_true, y_pred, average="micro"))
